59f167e63251757464d85a112b134dac3954f736
Mackin20 <macklinmacklin1@gmail.com>
2025-12-03 12:38:27 +0530

    changed exe.sh in main

--- AdventNet/aws/main/exe.sh
+++ AdventNet/aws/main/exe.sh
@@ -1,7 +1,7 @@
-javac --patch-module java.base="./AdventNet/aws/lib/wmsiotrace.jar:./AdventNet/aws/lib/iotrace.jar" -cp "./AdventNet/aws/lib/*" -d . ./MainServer.java
+javac --patch-module java.base="../lib/wmsiotrace.jar:../lib/iotrace.jar" -cp "../lib/*" -d . ./MainServer.java
 
-jar uf ./AdventNet/aws/lib/Custom.jar -C . MainServer.class
+jar uf ../lib/Custom.jar -C . MainServer.class
 
 rm MainServer.class
 
-java --patch-module java.base="./AdventNet/aws/lib/wmsiotrace.jar:./AdventNet/aws/lib/iotrace.jar" -cp "./AdventNet/aws/lib/*" MainServer
+java --patch-module java.base="../lib/wmsiotrace.jar:../lib/iotrace.jar" -cp "../lib/*" MainServer
